af7f7ec3ffde1350c2f16eaa3e276c3b4d171b2b
Festison <105289311+Festison@users.noreply.github.com>
2022-06-28 14:14:07 +0900

    백준 단계별 문제 풀기 2. 조건문

---
+++ 백준 단계별 문제 풀기 2. 조건문/1. 1330번 두수비교하기.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B;
+	cin >> A>> B;
+
+	if (A > B)
+	{
+		cout << ">" << endl;
+	} 
+	else if (A<B)
+	{
+		cout << "<" << endl;
+	}
+	else if (A==B)
+	{
+		cout << "==" << endl;
+	}
+
+	return 0;
+}
