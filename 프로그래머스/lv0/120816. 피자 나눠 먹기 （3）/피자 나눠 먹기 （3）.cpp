49d2a890006442fb62003b152886c9999b1e6403
Festison <105289311+Festison@users.noreply.github.com>
2023-01-18 14:13:01 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int slice, int n) 
+{
+    int answer = 0;
+    
+    if(n%slice==0)
+    {
+        answer=n/slice;
+    }
+    
+    else
+    {
+        answer=n/slice+1;
+    }
+
+    
+    return answer;
+}
