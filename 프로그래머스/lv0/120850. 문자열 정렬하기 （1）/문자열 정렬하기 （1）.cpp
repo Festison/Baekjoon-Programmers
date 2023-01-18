3e4483992a8ad78d2a8755a0e31abe13ca21d59e
Festison <105289311+Festison@users.noreply.github.com>
2023-01-18 14:21:22 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(string my_string) 
+{
+    vector<int> answer;
+    
+    for(int i=0; i<my_string.size(); i++)
+    {
+        if(my_string[i]>='0' && my_string[i]<='9')
+        {
+            answer.push_back(my_string[i]-'0');
+        }
+    }
+    
+    sort(answer.begin(),answer.end());
+    
+    return answer;
+}
