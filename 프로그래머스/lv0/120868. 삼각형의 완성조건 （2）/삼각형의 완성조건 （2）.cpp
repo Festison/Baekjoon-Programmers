d4d9699f81b2f0a6ed0f3c53e3f014ae43cc8a18
Festison <105289311+Festison@users.noreply.github.com>
2023-01-18 14:10:17 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> sides) 
+{
+    int answer = 0;
+    
+    sort(sides.begin(), sides.end());
+    
+    answer+=(sides[0]*2-1);
+    
+    return answer;
+}
