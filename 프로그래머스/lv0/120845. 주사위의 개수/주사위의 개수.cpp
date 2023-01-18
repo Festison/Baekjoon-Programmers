8f96c2bf97bc235cc31d11820fff129fa70b5178
Festison <105289311+Festison@users.noreply.github.com>
2023-01-18 14:21:06 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> box, int n) 
+{
+    int answer = 0;
+    
+    answer=(box[0]/n)*(box[1]/n)*(box[2]/n);
+
+    return answer;
+}
