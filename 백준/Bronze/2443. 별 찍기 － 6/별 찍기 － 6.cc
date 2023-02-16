2ffb7bf456057bfe7a032fae29c291eee8f485c7
Festison <105289311+Festison@users.noreply.github.com>
2023-02-16 14:40:46 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << " ";
+		}
+
+		for (int j = 1; j <= (2*(N-i))-1; j++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
