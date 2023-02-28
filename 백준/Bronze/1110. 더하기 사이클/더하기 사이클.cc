4b5487fcc72ee9127a8404eb2ebf8374af36519b
Festison <105289311+Festison@users.noreply.github.com>
2023-02-28 20:08:47 +0900

    [Bronze I] Title: 더하기 사이클, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int N;
+	int count = 0;
+	int num, temp;
+	cin >> N;
+	num = N;
+
+	while (true) 
+	{
+		temp = (N % 10) * 10 + (N / 10 + N % 10) % 10;
+		count++;
+		N = temp;
+		if (temp == num)
+			break;
+	}
+	cout << count << '\n';
+	return 0;
+}
