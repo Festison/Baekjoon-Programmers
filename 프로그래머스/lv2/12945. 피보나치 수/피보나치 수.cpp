9c6e6ae845e769f456919fc39dbb70ad1a821761
Festison <105289311+Festison@users.noreply.github.com>
2023-07-07 18:59:07 +0900

    [level 2] Title: 피보나치 수, Time: 0.51 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    int F[100001];
+    F[0]=0;
+    F[1]=1;
+    
+    for(int i=2; i<=n; i++) 
+    {
+        F[i]=(F[i-2] + F[i-1]) % 1234567;
+        
+        answer=F[n];;
+    }
+    
+    return answer;
+}
