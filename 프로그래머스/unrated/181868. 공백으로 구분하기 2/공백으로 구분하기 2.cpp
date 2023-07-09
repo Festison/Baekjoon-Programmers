ad37d6ca3c74eef65ff938b03298acda2442fb5e
Festison <105289311+Festison@users.noreply.github.com>
2023-07-09 17:52:18 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+vector<string> solution(string my_string) 
+{
+    vector<string> answer;
+    stringstream ss(my_string);
+    string word;
+    
+    while (ss >> word)
+    {
+        answer.push_back(word);
+    }
+    
+    return answer;
+}
