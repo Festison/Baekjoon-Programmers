1a07209ce3c2d4885dea371caee1e8e4ac8920b6
Festison <105289311+Festison@users.noreply.github.com>
2023-08-04 19:21:02 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 105.09 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) 
 {  
+    // 벡터의 중복된 원소를 제거하는 기능 #include <algorithm> 필요
     arr.erase(unique(arr.begin(),arr.end()),arr.end());
     
     vector<int> answer=arr;
