a9964a4073d2173ea4cfd97d8322b8f8bc1e608c
Festison <105289311+Festison@users.noreply.github.com>
2023-08-05 10:25:55 +0900

    [unrated] Title: 수 조작하기 2, Time: 1.00 ms, Memory: 5.89 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(vector<int> numLog) 
+{
+    string answer = "";
+    
+    for (int i = 1; i < numLog.size(); i++) 
+    {
+        int diff = numLog[i] - numLog[i - 1];
+        
+        if (diff == 1)
+        {
+            answer += 'w';
+        } 
+        else if (diff == -1) 
+        {
+            answer += 's';
+        } 
+        else if (diff == 10)
+        {
+            answer += 'd';
+        }
+        else if (diff == -10)
+        {
+            answer += 'a';
+        }
+    }
+    
+    return answer;
+}
